fbe25397f2e943702a86d87f97eaa7c09d0dedd8
sanj06 <52536720+sanj06@users.noreply.github.com>
2022-02-03 22:38:20 -0800

    fix indentation

--- starter/MyLinkedList.java
+++ starter/MyLinkedList.java
@@ -15,156 +15,156 @@ public class MyLinkedList<E> implements MyReverseList<E>{
     Node head;
     Node tail;
 
-	/**
+    /**
      * A Node class that holds data and references to previous and next Nodes
-	 * This class is used for both MyLinkedList and MyListIterator.
+     * This class is used for both MyLinkedList and MyListIterator.
      */
     protected class Node {
-	    E data;
-	    Node next;
-	    Node prev;
+        E data;
+        Node next;
+        Node prev;
 
-		/** 
-		 * Constructor to create singleton Node 
-		 * @param element Element to add, can be null	
-		 */
-		public Node(E element) {
-			//Initialise the elements
-			this.data = element;
-			this.next = null;
-			this.prev = null;
-		}
+        /** 
+         * Constructor to create singleton Node 
+         * @param element Element to add, can be null	
+         */
+        public Node(E element) {
+            //Initialise the elements
+            this.data = element;
+            this.next = null;
+            this.prev = null;
+        }
 
-		/** 
-		 * Set the previous node in the list
-		 * @param p new previous node
-		 */
-		public void setPrev(Node p) {
-			//Set the node p on the previous position
-			prev = p;
-		}
+        /** 
+         * Set the previous node in the list
+         * @param p new previous node
+         */
+        public void setPrev(Node p) {
+            //Set the node p on the previous position
+            prev = p;
+        }
 
-		/** 
-		 * Set the next node in the list
-		 * @param n new next node
-		 */
-		public void setNext(Node n) {
-			//Set the node n on the next position
-			next = n;
-		}
+        /** 
+         * Set the next node in the list
+         * @param n new next node
+         */
+        public void setNext(Node n) {
+            //Set the node n on the next position
+            next = n;
+        }
 
-		/** 
-		 * Set the element 
-		 * @param e new element 
-		 */
-		public void setElement(E e) {
-			this.data = e;
-		}
+        /** 
+         * Set the element 
+         * @param e new element 
+         */
+        public void setElement(E e) {
+            this.data = e;
+        }
 
-		/** 
-		 * Accessor to get the next Node in the list 
-		 * @return the next node
-		 */
-		public Node getNext() {
-			return this.next;
-		}
-		/** 
-		 * Accessor to get the prev Node in the list
-		 * @return the previous node  
-		 */
-		public Node getPrev() {
-			return this.prev;
-		} 
-		/** 
-		 * Accessor to get the Nodes Element 
-		 * @return this node's data
-		 */
-		public E getElement() {
-			return this.data;
-		} 
-	}
+        /** 
+         * Accessor to get the next Node in the list 
+         * @return the next node
+         */
+        public Node getNext() {
+            return this.next;
+        }
+        /** 
+         * Accessor to get the prev Node in the list
+         * @return the previous node  
+         */
+        public Node getPrev() {
+            return this.prev;
+        } 
+        /** 
+         * Accessor to get the Nodes Element 
+         * @return this node's data
+         */
+        public E getElement() {
+            return this.data;
+        } 
+    }
 
-	/**
-	 * Constructor to create an empty Doubly linked list
-	 */
-	public MyLinkedList(E[] arr) {
-		//Create dummy nodes
-		head = new Node(null);
-		tail = new Node(null);
-		head.setNext(tail);
-		tail.setPrev(head);
-		size = 0;	
+    /**
+     * Constructor to create an empty Doubly linked list
+     */
+    public MyLinkedList(E[] arr) {
+        //Create dummy nodes
+        head = new Node(null);
+        tail = new Node(null);
+        head.setNext(tail);
+        tail.setPrev(head);
+        size = 0;	
 
-		//create list by inserting each element
-		Node currNode = head;
-		for(int i=0; i<arr.length; i++){
-			Node newNode = new Node(arr[i]);
-			currNode.next.prev = newNode;
-			newNode.next = currNode.next;
-			newNode.prev = currNode;
-			currNode.next = newNode;
+        //create list by inserting each element
+        Node currNode = head;
+        for(int i=0; i<arr.length; i++){
+            Node newNode = new Node(arr[i]);
+            currNode.next.prev = newNode;
+            newNode.next = currNode.next;
+            newNode.prev = currNode;
+            currNode.next = newNode;
 
-			//move pointer to the next node
-			currNode = currNode.next;
-			//increase size of list
-			this.size++;
-		}
-	}
-
-
-	/**
-	 * TODO: Method header comment here
-	 */
-	public void reverseRegion(int fromIndex, int toIndex){
-		//TODO: Add your solution here
-	}
-
-	@Override
-	/** 
-	 * Returns the number of elements stored
-	 * @return - number of elements in the linkedlist
-	*/
-	public int size() {
-		//Return the number of nodes in the linkedlist
-		return this.size;
-	}
-
-	@Override
-	/** 
-	 * Get contents at position i
-	 * @param index - The index position to obtain the data
-	 * @return the Element at the specified index
-	 */
-	public E get(int index)	{
-
-		Node currNode = this.getNth(index);
-
-		//Get the value of data at the position
-		E element = currNode.getElement();
-
-		return element;	
-	}
+            //move pointer to the next node
+            currNode = currNode.next;
+            //increase size of list
+            this.size++;
+        }
+    }
 
 
-	/** A method that returns the node at a specified index,
-	 *  not the content
-	 *  @param index - position to return the node
-	 * @return - Node at 'index'
-	 */
-	// Helper method to get the Node at the Nth index
-	protected Node getNth(int index) {
-		if (index >= this.size || index < 0)
-			throw new IndexOutOfBoundsException();
+    /**
+     * TODO: Method header comment here
+     */
+    public void reverseRegion(int fromIndex, int toIndex){
+        //TODO: Add your solution here
+    }
 
-		Node currNode = this.head;
+    @Override
+    /** 
+     * Returns the number of elements stored
+     * @return - number of elements in the linkedlist
+    */
+    public int size() {
+        //Return the number of nodes in the linkedlist
+        return this.size;
+    }
 
-		//Loop through the linked list and stop at the position
-		for (int i = 0; i <= index; i++) {
-			currNode = currNode.getNext();
-		}
+    @Override
+    /** 
+     * Get contents at position i
+     * @param index - The index position to obtain the data
+     * @return the Element at the specified index
+     */
+    public E get(int index)	{
 
-		//return the node	
-		return currNode; 
-	}
+        Node currNode = this.getNth(index);
+
+        //Get the value of data at the position
+        E element = currNode.getElement();
+
+        return element;	
+    }
+
+
+    /** A method that returns the node at a specified index,
+     *  not the content
+     *  @param index - position to return the node
+     * @return - Node at 'index'
+     */
+    // Helper method to get the Node at the Nth index
+    protected Node getNth(int index) {
+        if (index >= this.size || index < 0)
+            throw new IndexOutOfBoundsException();
+
+        Node currNode = this.head;
+
+        //Loop through the linked list and stop at the position
+        for (int i = 0; i <= index; i++) {
+            currNode = currNode.getNext();
+        }
+
+        //return the node	
+        return currNode; 
+    }
 
 }
